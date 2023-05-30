672457ce977a961d871844c0ffa13a4b8fd94489
Farhad Zada <farhad.szd@gmail.com>
2023-05-30 12:58:32 +0400

    1.0.0

--- contracts/LevelsStaking3.sol
+++ contracts/LevelsStaking3.sol
@@ -11,30 +11,24 @@ import {ILevels} from "./ILevels.sol";
 
 error Staking__NotAdmin();
 error Staking__NotAvailable(uint256 amount);
+error Staking__MinStakingAmount(uint256 amount);
+error Staking__MaxStakingAmount(uint256 amount);
+error Staking__PoolMaxSize(uint256 amount);
 error Staking__StatusNotActive();
-error Staking__InvalidTokenId(uint256 tokenId);
 error Staking__InvalidId();
 error Staking__InsufficientFunds(uint balance);
 error Staking__ContractLacksBalance();
-error Staking__TransferFailed();
 error Staking__NotEnoughAllowance();
 
-contract AdvantaStaking is Initializable, OwnableUpgradeable, UUPSUpgradeable {
-
+contract LevelsStaking is Initializable, OwnableUpgradeable, UUPSUpgradeable {
     IERC20Upgradeable public token;
-    IERC721Upgradeable public land;
-    IERC721Upgradeable public metahut;
     ILevels public levels;
 
     bool public canActivateStaking = false;
 
     uint256 public totalStakedToken;
-    uint256 public totalStakedLand;
-    uint256 public totalStakedMetahut;
 
     uint256 public rewardPercentage;
-    uint256 public rewardPercentageLand;
-    uint256 public rewardPercentageMetahut;
     uint256 public penaltyPercentage;
     uint256 public rewardDeadlineSeconds;
 
@@ -42,12 +36,6 @@ contract AdvantaStaking is Initializable, OwnableUpgradeable, UUPSUpgradeable {
     uint256 public minStakingAmount;
     uint256 public penaltyDivisionStep;
 
-
-    enum NftType {
-        NONE,
-        LAND,
-        METAHUT
-    }
     enum StakeStatus {
         WAITING,
         ACTIVE,
@@ -58,8 +46,6 @@ contract AdvantaStaking is Initializable, OwnableUpgradeable, UUPSUpgradeable {
     StakeStatus public stakeStatus;
 
     struct Staking {
-        NftType nftType;
-        uint256 nftId;
         uint256 amount;
         uint256 percent;
         uint256 stakedAt;
@@ -69,18 +55,13 @@ contract AdvantaStaking is Initializable, OwnableUpgradeable, UUPSUpgradeable {
         uint64 level;
         uint256 levelReward;
         uint256 levelMaxStakingAmount;
-
     }
 
     mapping(address => bool) private admins;
     mapping(address => Staking[]) private stakings;
-    mapping(NftType => IERC721Upgradeable) internal _typeToNft;
-    mapping(address => mapping(uint256 => address)) private owners;
-    mapping(address => mapping(address => uint256[])) private ownings;
     mapping(address => bool) private _nonReentrant;
     ////
     mapping(uint64 => LevelDetails) public levelDetails;
-
 
     event Stake(address indexed staker, Staking staking);
     event Claim(address indexed staker, Staking staking);
@@ -89,13 +70,15 @@ contract AdvantaStaking is Initializable, OwnableUpgradeable, UUPSUpgradeable {
 
     modifier stakeAvailable(address _staker, uint256 _amount) {
         if (_amount < minStakingAmount)
-            revert Staking__NotAvailable({amount: _amount});
+            revert Staking__MinStakingAmount({amount: _amount});
         if (totalStakedToken + _amount > poolMaxSize)
-            revert Staking__NotAvailable({amount: totalStakedToken + _amount});
+            revert Staking__PoolMaxSize({amount: totalStakedToken + _amount});
         if (stakeStatus != StakeStatus.ACTIVE)
             revert Staking__StatusNotActive();
-        if (getUserTotalStakedTokenAmount(_staker) + _amount > levelDetails[levels.getLevel(msg.sender)].levelMaxStakingAmount)
-            revert Staking__NotAvailable({amount: _amount});
+        if (
+            getUserTotalStakedTokenAmount(_staker) + _amount >
+            levelDetails[levels.getLevel(msg.sender)].levelMaxStakingAmount
+        ) revert Staking__MaxStakingAmount({amount: _amount});
         _;
     }
 
@@ -111,68 +94,41 @@ contract AdvantaStaking is Initializable, OwnableUpgradeable, UUPSUpgradeable {
     }
 
     constructor() {
-            _disableInitializers();
+        _disableInitializers();
     }
 
-    function _authorizeUpgrade(address newImplementation)
-        internal
-        onlyOwner
-        override
-    {}
+    function _authorizeUpgrade(
+        address newImplementation
+    ) internal override onlyOwner {}
 
     // Init
 
-    function initialize(
-        address _token,
-        address _levels,
-        address _land,
-        address _metahut
-    ) public initializer {
+    function initialize(address _token, address _levels) public initializer {
         __Ownable_init();
         admins[msg.sender] = true;
         token = IERC20Upgradeable(_token);
-        land = IERC721Upgradeable(_land);
-        metahut = IERC721Upgradeable(_metahut);
-        _typeToNft[NftType.LAND] = land;
-        _typeToNft[NftType.METAHUT] = metahut;
         levels = ILevels(_levels);
 
         rewardPercentage = 0;
-        rewardPercentageLand = 0;
-        rewardPercentageMetahut = 0;
         penaltyPercentage = 10;
-        rewardDeadlineSeconds = 3600 * 24 * 30 * 6; // 3 months
-        poolMaxSize = 25_000_000 * 10e18;
-        minStakingAmount = 20_000 * 10e18;
+        rewardDeadlineSeconds = 3600 * 24 * 30 * 3; //  a year
+        poolMaxSize = 50_000_000 * 10 ** 18;
+        minStakingAmount = 20_000 * 10 ** 18;
         penaltyDivisionStep = 30 * 3;
     }
 
     function stake(
         uint256 _amount
     ) public nonReentrant stakeAvailable(msg.sender, _amount) {
-        _stake(NftType.NONE, 0, _amount, msg.sender, msg.sender, true);
+        _stake(_amount, msg.sender, msg.sender, true);
     }
 
     function claim(uint256 _id) public nonReentrant {
-        _claim(msg.sender, _id, NftType.NONE);
+        _claim(msg.sender, _id);
     }
 
-    function _remove(
-        address _staker,
-        uint256 _index,
-        uint256 _nftId,
-        address _nft
-    ) internal {
+    function _remove(address _staker, uint256 _index) internal {
         delete stakings[_staker][_index];
-        if (_nft != address(0)) {
-            delete owners[_nft][_nftId];
-            for (uint i = 0; i < ownings[_nft][_staker].length; i++) {
-                if (ownings[_nft][_staker][i] == _nftId) {
-                    delete ownings[_nft][_staker][i];
-                    return;
-                }
-            }
-        }
     }
 
     function setStakingStatus(StakeStatus status) public onlyOwner {
@@ -180,7 +136,7 @@ contract AdvantaStaking is Initializable, OwnableUpgradeable, UUPSUpgradeable {
         stakeStatus = status;
     }
 
-    function setLevelDetails (
+    function setLevelDetails(
         uint64 _level,
         uint256 _rewardPercent,
         uint256 _maxStakingAmount
@@ -188,10 +144,18 @@ contract AdvantaStaking is Initializable, OwnableUpgradeable, UUPSUpgradeable {
         if (!canActivateStaking) {
             canActivateStaking = true;
         }
-        levelDetails[_level] = LevelDetails({level: _level, levelReward: _rewardPercent, levelMaxStakingAmount: _maxStakingAmount});
+        levelDetails[_level] = LevelDetails({
+            level: _level,
+            levelReward: _rewardPercent,
+            levelMaxStakingAmount: _maxStakingAmount
+        });
     }
 
-    function modifyStakingDetails(uint256 _poolMaxSize, uint256 _minStakingAmount, uint256 _penaltyDivisionSteps) public admin {
+    function modifyStakingDetails(
+        uint256 _poolMaxSize,
+        uint256 _minStakingAmount,
+        uint256 _penaltyDivisionSteps
+    ) public admin {
         if (_poolMaxSize > 0) {
             poolMaxSize = _poolMaxSize;
         }
@@ -201,8 +165,6 @@ contract AdvantaStaking is Initializable, OwnableUpgradeable, UUPSUpgradeable {
         if (_penaltyDivisionSteps > 0) {
             penaltyDivisionStep = _penaltyDivisionSteps;
         }
-
-
     }
 
     function withdraw(address payable who, uint amount) external onlyOwner {
@@ -220,8 +182,6 @@ contract AdvantaStaking is Initializable, OwnableUpgradeable, UUPSUpgradeable {
     // Helpers
 
     function _stake(
-        NftType nftType,
-        uint256 nftId,
         uint256 _amount,
         address staker,
         address sponsor,
@@ -232,8 +192,6 @@ contract AdvantaStaking is Initializable, OwnableUpgradeable, UUPSUpgradeable {
             revert Staking__InsufficientFunds({balance: _balance});
 
         Staking memory staking = Staking({
-            nftType: nftType,
-            nftId: nftId,
             amount: _amount,
             percent: 0,
             stakedAt: block.timestamp
@@ -246,18 +204,6 @@ contract AdvantaStaking is Initializable, OwnableUpgradeable, UUPSUpgradeable {
 
         if (withLevel) {
             staking.percent = levelDetails[levels.getLevel(staker)].levelReward;
-        } else if (nftType == NftType.LAND) {
-            land.transferFrom(sponsor, address(this), nftId);
-            totalStakedLand += 1;
-            owners[address(land)][nftId] = staker;
-            ownings[address(land)][staker].push(nftId);
-            staking.percent = rewardPercentageLand;
-        } else if (nftType == NftType.METAHUT) {
-            metahut.transferFrom(sponsor, address(this), nftId);
-            totalStakedMetahut += 1;
-            owners[address(metahut)][nftId] = staker;
-            ownings[address(metahut)][staker].push(nftId);
-            staking.percent = rewardPercentageMetahut;
         } else {
             staking.percent = rewardPercentage;
         }
@@ -265,9 +211,9 @@ contract AdvantaStaking is Initializable, OwnableUpgradeable, UUPSUpgradeable {
         emit Stake(staker, staking);
     }
 
-    function _claim(address staker, uint256 _id, NftType nftType) internal {
+    function _claim(address staker, uint256 _id) internal {
         uint256 _balance = token.balanceOf(address(this));
-        int256 _index = _getStakeIndexById(staker, nftType, _id);
+        int256 _index = _getStakeIndexById(staker, _id);
         if (_index < 0) revert Staking__InvalidId();
         uint256 index = uint256(_index);
 
@@ -283,17 +229,7 @@ contract AdvantaStaking is Initializable, OwnableUpgradeable, UUPSUpgradeable {
 
         Staking memory staking = stakings[staker][uint256(index)];
 
-        if (staking.nftType == NftType.LAND) {
-            land.transferFrom(address(this), staker, staking.nftId);
-            _remove(staker, index, staking.nftId, address(land));
-            totalStakedLand -= 1;
-        } else if (staking.nftType == NftType.METAHUT) {
-            metahut.transferFrom(address(this), staker, staking.nftId);
-            _remove(staker, index, staking.nftId, address(metahut));
-            totalStakedMetahut -= 1;
-        } else {
-            _remove(staker, index, staking.nftId, address(0));
-        }
+        _remove(staker, index);
 
         emit Claim(staker, staking);
     }
@@ -305,24 +241,21 @@ contract AdvantaStaking is Initializable, OwnableUpgradeable, UUPSUpgradeable {
         uint256 secondsStaked
     ) internal view returns (uint) {
         uint256 chunkSize = rewardDeadlineSeconds / penaltyDivisionStep;
-        uint256 chunkPercent = (penaltyPercentage * 10e10) /
+        uint256 chunkPercent = (penaltyPercentage * 10 ** 10) /
             penaltyDivisionStep;
         uint256 percent = penaltyPercentage *
-            10e10 -
+            10 ** 10 -
             ((secondsStaked / chunkSize) * chunkPercent);
-        return amount - (((amount * percent) / 100) / 10e10);
+        return amount - (((amount * percent) / 100) / 10 ** 10);
     }
 
     function _getStakeIndexById(
         address _staker,
-        NftType _nftType,
         uint256 _id
     ) internal view returns (int256) {
         Staking[] memory _stakings = stakings[_staker];
         for (uint256 i = 0; i < _stakings.length; i++) {
-            if (
-                _stakings[i].stakedAt == _id && _stakings[i].nftType == _nftType
-            ) {
+            if (_stakings[i].stakedAt == _id) {
                 return int(i);
             }
         }
@@ -339,7 +272,10 @@ contract AdvantaStaking is Initializable, OwnableUpgradeable, UUPSUpgradeable {
         if (secondsStaked < rewardDeadlineSeconds) {
             return (_getPenalty(staking.amount, secondsStaked), staking.amount);
         }
-        return ((staking.amount * staking.percent) / 100, staking.amount);
+        return (
+            (staking.amount * staking.percent) / 100 + staking.amount,
+            staking.amount
+        );
     }
 
     // State read funcs
@@ -357,7 +293,6 @@ contract AdvantaStaking is Initializable, OwnableUpgradeable, UUPSUpgradeable {
     function getUserTotalStakedTokenAmount(
         address staker
     ) public view returns (uint256) {
-        require(msg.sender == staker || msg.sender == owner(), "Unallowed");
         Staking[] memory _stakings = stakings[staker];
         uint256 total;
         for (uint i = 0; i < _stakings.length; i++) {
@@ -367,30 +302,30 @@ contract AdvantaStaking is Initializable, OwnableUpgradeable, UUPSUpgradeable {
     }
 
     function getStakeById(
-        NftType _nftType,
         uint256 _id
     ) public view returns (Staking memory staking) {
         Staking[] memory _stakings = stakings[msg.sender];
         for (uint256 i = 0; i < _stakings.length; i++) {
-            if (
-                _stakings[i].stakedAt == _id && _stakings[i].nftType == _nftType
-            ) {
+            if (_stakings[i].stakedAt == _id) {
                 return _stakings[i];
             }
         }
-        return Staking(NftType.NONE, 0, 0, 0, 0);
+        return Staking(0, 0, 0);
     }
 
     function getTransferAmount(
-        uint256 _index
+        uint256 _id
     ) public view returns (uint256, uint256) {
-        Staking memory staking = stakings[msg.sender][_index];
         uint256 timestamp = block.timestamp;
+        Staking memory staking = getStakeById(_id);
         uint256 secondsStaked = timestamp - staking.stakedAt;
         if (secondsStaked < rewardDeadlineSeconds) {
             return (_getPenalty(staking.amount, secondsStaked), staking.amount);
         }
-        return ((staking.amount * staking.percent) / 100, staking.amount);
+        return (
+            (staking.amount * staking.percent) / 100 + staking.amount,
+            staking.amount
+        );
     }
 
     function isAdminTrue() public view returns (bool) {
@@ -402,11 +337,10 @@ contract AdvantaStaking is Initializable, OwnableUpgradeable, UUPSUpgradeable {
     // admin sets specific reward percent for user
     function modifyRewardPersentage(
         address staker,
-        NftType nftType,
         uint256 id,
         uint256 newPercent
     ) public admin {
-        int256 _index = _getStakeIndexById(staker, nftType, id);
+        int256 _index = _getStakeIndexById(staker, id);
         if (_index < 0) revert Staking__InvalidId();
         uint256 index = uint256(_index);
 
@@ -416,21 +350,15 @@ contract AdvantaStaking is Initializable, OwnableUpgradeable, UUPSUpgradeable {
     // admin sets stake for a specific user
     function setStake(
         address staker,
-        NftType nftType,
-        uint256 nftId,
         uint256 _amount,
         address sponsor,
         bool withLevel
     ) public admin {
-        _stake(nftType, nftId, _amount, staker, sponsor, withLevel);
+        _stake(_amount, staker, sponsor, withLevel);
     }
 
     // admin claims a specific user
-    function claimFor(
-        address staker,
-        uint256 _id,
-        NftType nftType
-    ) public admin {
-        _claim(staker, _id, nftType);
+    function claimFor(address staker, uint256 _id) public admin {
+        _claim(staker, _id);
     }
 }
--- scripts/deploy.js
+++ scripts/deploy.js
@@ -1,16 +1,16 @@
 // import { ethers, upgrades } from "hardhat";
 async function main() {
-    const Land = await ethers.getContractFactory("Land")
+  const LevelsStaking = await ethers.getContractFactory("LevelsStaking");
 
-    const land = await upgrades.deployProxy(Land);
-    // Start deployment, returning a promise that resolves to a contract object
-    await land.deployed()
-    console.log("Contract deployed to address:", land.address)
-  }
-  
-  main()
-    .then(() => process.exit(0))
-    .catch((error) => {
-      console.error(error)
-      process.exit(1)
-    })
+  const levelsStaking = await upgrades.deployProxy(LevelsStaking);
+  // Start deployment, returning a promise that resolves to a contract object
+  await levelsStaking.deployed();
+  console.log("Contract deployed to address:", levelsStaking.address);
+}
+
+main()
+  .then(() => process.exit(0))
+  .catch((error) => {
+    console.error(error);
+    process.exit(1);
+  });
